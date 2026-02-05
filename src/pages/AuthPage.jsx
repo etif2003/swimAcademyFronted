c9d1ebda76cdfc5565038a180b2a51b401fc170e
chavi <chavichaimson@gmail.com>
2026-02-05 11:00:08 +0200

    login and register css

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,25 +1,9 @@
-// import { useSearchParams } from "react-router";
-// import LoginForm from "../components/LoginForm";
-// import RegisterForm from "../components/RegisterForm";
 
-// const AuthPage = () => {
-//   const [searchParams] = useSearchParams();
 
-//   const mode = searchParams.get("mode"); // signup | null
-//   const role = searchParams.get("role"); // instructor | school | null
-
-//   if (mode === "signup") {
-//     return <RegisterForm role={role} />;
-//   }
-
-//   return <LoginForm />;
-// };
-
-// export default AuthPage;
 
 import { useSearchParams } from "react-router";
-import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
+import LoginForm from "../components/LoginForm";
 import AuthTabs from "../components/AuthTabs";
 
 const AuthPage = () => {
